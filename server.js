abf553eccc5c64c5daec660c06baecb49f0eab58
Aaron Freeman <Freeman0483@gmail.com>
2018-04-08 18:32:08 -0700

    Mongoose Blog App

--- server.js
+++ server.js
@@ -1,26 +1,128 @@
+'use-strict';
+
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+
+const { DATABASE_URL, PORT } = require('./config');
+const { BlogPost } = require('./models');
 
 const app = express();
 
-const blogPosts = require('./blogRouter');
+// const blogPosts = require('./blogRouter');
 
 app.use(morgan('common'));
-app.use('/blog-posts', blogPosts);
+app.use(bodyParser.json());
+// app.use('/blog-posts', blogPosts);
+
+app.get('/posts', (req, res) => {
+  BlogPost
+    .find()
+    .then(posts => {
+      res.json(posts.map(post => post.serialize()));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'something went terribly wrong' });
+    });
+});
+
+app.get('/posts/:id', (req, res) => {
+  console.log(req) // just to look at it.
+  console.log(req.body) // check it out
+  console.log(req.params) // look at this too
+  BlogPost
+    .findById(req.params.id)
+    .then(post => {
+      console.log(post) // just to look at it
+      res.json(post);
+    })
+    .catch(err => {
+      // write code to handle error
+    });
+});
+
+app.post('/posts', (req, res) => {
+
+  const requiredFields = ['author', 'created', 'id', 'title', 'content'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if(!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  BlogPost
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author,
+      created: req.body.created,
+      id: req.body.id
+    })
+    .then(restaurant => res.status(200).json(restaurant.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
+
+app.put('/posts/:id', (req, res) => {
+  // handle the case if the client sends over an invalid :id
+
+  const fieldsToUpdate = ['title', 'author', 'content'];
+  let updatedDocument = {};
+  fieldsToUpdate.forEach(thing => {
+    if (thing in req.body) {
+      updatedDocument[thing] = req.body[thing];
+    }
+  });
+
+  BlogPost
+    .findByIdAndUpdate(req.params.id, updatedDocument)
+    .then(updatedPost => {
+      res.json(updatedPost);
+    })
+    .catch(err => res.status(500).json({ message: 'Internal server error'}));
+    });
+
+app.delete('/posts/:id', (req, res) => {
+  BlogPost
+    .findByIdAndRemove(req.params.id)
+    .then(post => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal Server Error'}));
+});
+
+app.use('*', function (req, res) {
+  res.status(404).json({ message: 'Not Found' });
+});
 
 let server;
 
-function runServer() {
-  const port = process.env.PORT || 8080;
+
+function runServer(databaseUrl, port=PORT) {
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
     });
   });
 }
+
 
 function closeServer() {
   return new Promise((resolve, reject) => {
@@ -36,7 +138,7 @@ function closeServer() {
 }
 
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+  runServer(DATABASE_URL).catch(err => console.error(err));
 };
 
 module.exports = {app, runServer, closeServer};
--- test/test-blog-router.js
+++ test/test-blog-router.js
@@ -1,72 +1,72 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-
-const {app, runServer, closeServer} = require('../server');
-const expect = chai.expect;
-chai.use(chaiHttp);
-
-describe('BlogPosts', function() {
-  before(function() {
-    return runServer();
-  });
-  after(function() {
-    return closeServer();
-  });
-
-  it('should show blog-post on GET', function() {
-    return chai.request(app)
-      .get('/blog-posts')
-      .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('array');
-        expect(res.body.length).to.be.at.least(1);
-        const expectedKeys = ['title', 'content', 'author', 'publishDate'];
-        res.body.forEach(function(item) {
-          expect(item).to.be.a('object');
-          expect(item).to.include.keys(expectedKeys);
-        });
-      });
-  });
-  it('should add blog-post on POST', function() {
-    const newBlog = {title: 'something', content: 'anything', author: 'some guy', publishDate: '2012'};
-    return chai.request(app)
-      .post('/blog-posts')
-      .send(newBlog)
-      .then(function(res) {
-        expect(res).to.have.status(201);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('object');
-        expect(res.body).to.include.keys('title', 'content', 'author', 'publishDate');
-        expect(res.body.id).to.not.equal(null);
-        expect(res.body).to.deep.equal(Object.assign(newBlog, {id: res.body.id}));
-      });
-  });
-  it('should update blog posts on PUT', function() {
-    return chai.request(app)
-      .get('/blog-posts')
-      .then(function( res) {
-        const updatedPost = Object.assign(res.body[0], {
-          title: 'c;ldms',
-          content: 'slkmvdklw;'
-        });
-        return chai.request(app)
-          .put(`/blog-posts/${res.body[0].id}`)
-          .send(updatedPost)
-          .then(function(res) {
-            expect(res).to.have.status(204);
-          });
-      });
-  });
-  it('should delete blog-post on DELETE', function() {
-    return chai.request(app)
-      .get('/blog-posts')
-      .then(function(res) {
-        return chai.request(app)
-          .delete(`/blog-posts/${res.body[0].id}`);
-      })
-      .then(function(res) {
-        expect(res).to.have.status(204);
-      });
-  });
-});
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+//
+// const {app, runServer, closeServer} = require('../server');
+// const expect = chai.expect;
+// chai.use(chaiHttp);
+//
+// describe('BlogPosts', function() {
+//   before(function() {
+//     return runServer();
+//   });
+//   after(function() {
+//     return closeServer();
+//   });
+//
+//   it('should show blog-post on GET', function() {
+//     return chai.request(app)
+//       .get('/blog-posts')
+//       .then(function(res) {
+//         expect(res).to.have.status(200);
+//         expect(res).to.be.json;
+//         expect(res.body).to.be.a('array');
+//         expect(res.body.length).to.be.at.least(1);
+//         const expectedKeys = ['title', 'content', 'author', 'publishDate'];
+//         res.body.forEach(function(item) {
+//           expect(item).to.be.a('object');
+//           expect(item).to.include.keys(expectedKeys);
+//         });
+//       });
+//   });
+//   it('should add blog-post on POST', function() {
+//     const newBlog = {title: 'something', content: 'anything', author: 'some guy', publishDate: '2012'};
+//     return chai.request(app)
+//       .post('/blog-posts')
+//       .send(newBlog)
+//       .then(function(res) {
+//         expect(res).to.have.status(201);
+//         expect(res).to.be.json;
+//         expect(res.body).to.be.a('object');
+//         expect(res.body).to.include.keys('title', 'content', 'author', 'publishDate');
+//         expect(res.body.id).to.not.equal(null);
+//         expect(res.body).to.deep.equal(Object.assign(newBlog, {id: res.body.id}));
+//       });
+//   });
+//   it('should update blog posts on PUT', function() {
+//     return chai.request(app)
+//       .get('/blog-posts')
+//       .then(function( res) {
+//         const updatedPost = Object.assign(res.body[0], {
+//           title: 'c;ldms',
+//           content: 'slkmvdklw;'
+//         });
+//         return chai.request(app)
+//           .put(`/blog-posts/${res.body[0].id}`)
+//           .send(updatedPost)
+//           .then(function(res) {
+//             expect(res).to.have.status(204);
+//           });
+//       });
+//   });
+//   it('should delete blog-post on DELETE', function() {
+//     return chai.request(app)
+//       .get('/blog-posts')
+//       .then(function(res) {
+//         return chai.request(app)
+//           .delete(`/blog-posts/${res.body[0].id}`);
+//       })
+//       .then(function(res) {
+//         expect(res).to.have.status(204);
+//       });
+//   });
+// });
